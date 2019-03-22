f5758dbd99cfe7f23f8ac23872ea76c0e715585b
Juan Manuel Garcia <jumagarilla@hotmail.com>
2019-03-22 16:09:11 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose=require('mongoose');
 
 const app=express();
 
+const routeFunc=require("./routes/routes");
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -14,25 +16,5 @@ app.use((req, res, next) => {
 
 app.use(body_parser.json());
 
-app.use(function(req,res,next){
-    console.log('Server is running...');
-    next();
-});
-app.get('/graphs',function(req, res,next) {
-    console.log('Output posted in a different route!!!');
-    res.status(401).json({
-        Hello: 'Hello World',
-        message: 'You are in graphs now'
-    }).end();
-    
-});
-
-app.post('/graphs',function(req,res,next) {
-    res.status(201).json({
-        data: req.body
-    });
-    next();
-    console.log("Data posted");
-});
-
+app.use("/graphs",routeFunc);
 module.exports=app;
